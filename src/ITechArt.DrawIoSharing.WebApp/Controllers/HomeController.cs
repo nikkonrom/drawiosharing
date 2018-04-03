d1395e1c279a64e3565fb97d4a1f0f9e0d3f2528
Никита Романюк <nikkonrom@gmail.com>
2018-04-03 15:36:21 +0300

    Removed unused scripts

--- src/ITechArt.DrawIoSharing.WebApp/Controllers/HomeController.cs
+++ src/ITechArt.DrawIoSharing.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using ITechArt.Common.Logging;
 using ITechArt.DrawIoSharing.WebApp.Filters;
 
@@ -19,6 +21,27 @@ namespace ITechArt.DrawIoSharing.WebApp.Controllers
         public ViewResult Index()
         {
             return View();
+        }
+
+        public ActionResult ChangeCulture(string lang)
+        {
+            var returnUrl = Request.UrlReferrer.AbsolutePath;
+            var cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;
+            else
+            {
+
+                cookie = new HttpCookie("lang")
+                {
+                    HttpOnly = false,
+                    Value = lang,
+                    Expires = DateTime.Now.AddYears(1)
+                };
+            }
+            Response.Cookies.Add(cookie);
+
+            return Redirect(returnUrl);
         }
     }
 }
